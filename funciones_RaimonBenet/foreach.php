cc459223d5cc49a83944b3fbf3b5e82e1276de73
RaimonBN <raimonbn2001@gmail.com>
2021-10-06 09:19:48 +0200

    Foreach 1 y 2

---
+++ funciones_RaimonBenet/foreach.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+<body>
+    <?php
+    echo "<ul>";
+    $color = array('blanco', 'verde', 'rojo');
+    foreach($color as $valor){
+        echo "<li>$valor</li>";
+    }
+    
+    echo "</ul>";
+
+
+?>
+</body>
+</html>
